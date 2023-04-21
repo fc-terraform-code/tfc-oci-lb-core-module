4997563e4d47b814824a802cb6c0e66c0b302f53
facundocayon <facundocayon@gmail.com>
2023-04-21 11:10:03 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,7 @@
-# Copyright (c) 2020 Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-
-variable "tenancy_id" {}
-variable "user_id" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "private_key" {}
+variable "compartment_ocid" {}
 variable "region" {}
-variable "default_compartment_id" {}
+variable "fingerprint" {}
+variable "compartment_id" { type = string }
