b1f9280e7793977d10ac725f7f22ad8485392b94
facundocayon <facundocayon@gmail.com>
2023-04-21 11:42:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "oci_load_balancer_load_balancer" "this" {
   display_name   = "fc-test-lb"
   shape          = "10Mbps"
   # can't really provide a default value here, so no need for additional logic (subnets must be user-defined)
-  subnet_ids                 = ocid1.subnet.oc1.iad.aaaaaaaat3oydi7ftmagvevgzxczhhpbhtqp3ytadnv2l3uelckxnjsgjm2q
+  subnet_ids                 = "subnet-20210204-0026"
   is_private                 = true
   #network_security_group_ids = var.lb_options.nsg_ids != null ? var.lb_options.nsg_ids : local.lb_options_defaults.nsg_ids
   #defined_tags               = var.lb_options.defined_tags != null ? var.lb_options.defined_tags : local.lb_options_defaults.defined_tags
