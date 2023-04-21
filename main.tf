3b44a28e28f0b37ee6d6c00a528b2d1a75131867
facundocayon <facundocayon@gmail.com>
2023-04-21 11:12:12 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
-# Copyright (c) 2020 Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-
-module "oci_lb" {
-  source                  = "../../"
-  
-  default_compartment_id  = var.default_compartment_id
+module "oci_lb" {  
+  default_compartment_id  = var.default_compartment_ocid
   
   lb_options            = {
     display_name        = "test_lb"
     compartment_id      = null
-    shape               = "100Mbps"
+    shape               = "10Mbps"
     subnet_ids          = [oci_core_subnet.this.id]
     private             = true
     nsg_ids             = null
@@ -22,13 +16,13 @@ module "oci_lb" {
 resource "oci_core_vcn" "this" {
   dns_label             = "temp"
   cidr_block            = "192.168.0.0/16"
-  compartment_id        = var.default_compartment_id
+  compartment_id        = var.compartment_id
   display_name          = "temp"
 }
 
 resource "oci_core_subnet" "this" {
   cidr_block            = "192.168.0.0/24"
-  compartment_id        = var.default_compartment_id
+  compartment_id        = var.compartment_id
   vcn_id                = oci_core_vcn.this.id
 
   display_name          = "test"
