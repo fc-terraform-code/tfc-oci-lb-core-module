a00f8d131a095025c40400e33fccd80d3785507e
facundocayon <facundocayon@gmail.com>
2023-04-22 09:10:10 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "tenancy_ocid" { default = "ocid1.tenancy.oc1..aaaaaaaa56r5fsoqjyefyfkbktxmwk4eblqx473uwbrjvrjmzb43t7wakg6q" }
+variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "private_key" {}
 variable "region" {}
