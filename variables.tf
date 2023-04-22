dd87f2a7248d77c6111cb11a5de8cb15211dad3f
facundocayon <facundocayon@gmail.com>
2023-04-22 13:42:30 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,26 @@
 variable "tenancy_ocid" {}
+
 variable "user_ocid" {}
+
 variable "private_key" {}
+
 variable "region" {}
+
 variable "fingerprint" {}
+
 variable "compartment_ocid" { type = string }
+
 variable "organization" {
   type    = string
-  default = "tron_dataware"
+  default = ""
 }
+
 variable "name" {
   type    = string
-  default = "lab_tfc_github_lb_oci"
+  default = ""
 }
+
 variable "display_name" {
   type    = string
-  default = "fc-test-lb-2"
+  default = ""
 }
