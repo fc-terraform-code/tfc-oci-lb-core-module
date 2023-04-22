0d715eb0686b0ba824b675395e7d9e96673f1ecb
facundocayon <facundocayon@gmail.com>
2023-04-22 10:49:02 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 #--GitHub Actions & Terraform Cloud config
 terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 4.101.0"
+    }
+  }
   cloud {
     organization = var.organization
 
